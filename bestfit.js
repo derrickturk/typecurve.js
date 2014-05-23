c93f9e3b65beba634f00ee3b2e175d39d25a8640
Derrick W. Turk <dwt@terminusdatascience.com>
2014-05-23 12:59:46 -0500

    Having hell making gradient descent converge.

--- bestfit.js
+++ bestfit.js
@@ -5,8 +5,8 @@
     {
         var best_params = gradient_descent(function (qi, Di, b) {
             return sse(new ns.Hyperbolic(qi, Di, b), rate, time)
-        }, [rate[0], (rate[1] - rate[0]) / (time[1] - time[0]), 1.5],
-        [0.0, 0.0, 0.0], [1e99, 20.0, 5.0], 0.01, 1e-3, 0.01
+        }, [rate[0], (rate[0] - rate[1]) / (time[1] - time[0]) / rate[0], 1.5],
+        [0.0, 0.0, 0.0], [1e99, 20.0, 5.0], 0.001, 1e-3, [1, 0.0001, 0.001]
         )
 
         return new ns.Hyperbolic(
@@ -17,8 +17,8 @@
     {
         var best_params = gradient_descent(function (qi, Di, b) {
             return sse_interval(new ns.Hyperbolic(qi, Di, b), vol, time)
-        }, [vol[0], (vol[1] - vol[0]) / (time[1] - time[0]), 1.5],
-        [0.0, 0.0, 0.0], [1e99, 20.0, 5.0], 0.01, 1e-3, 0.01
+        }, [vol[0], (vol[0] - vol[1]) / (time[1] - time[0]) / vol[0], 1.5],
+        [0.0, 0.0, 0.0], [1e99, 20.0, 5.0], 0.01, 1e-3, [1, 0.0001, 0.001]
         )
 
         return new ns.Hyperbolic(
@@ -28,10 +28,13 @@
     function gradient_descent(f, guess, lower, upper, step, eps, grad_step)
     {
         var diff = Infinity,
-            last = f.apply(guess)
+            last = f.apply(null, guess)
+
+        var i = 0
 
         while (diff > eps) {
             var grad = gradient(f, guess, grad_step)
+            console.log('grad = ' + grad)
             guess = guess.map(function (val, i) {
                 return val - step * grad[i]
             })
@@ -42,9 +45,12 @@
                     return upper[i]
                 return val
             })
-            var next = f.apply(guess)
+            var next = f.apply(null, guess)
             diff = Math.abs(next - last)
             last = next
+
+            if (i++ > 1000)
+                break
         }
 
         return guess
@@ -52,12 +58,14 @@
 
     function gradient(f, x, h)
     {
-        var forward = x.map(function (i) { return i + h / 2.0 }),
-            backward = x.map(function (i) { return i - h / 2.0 })
-            f_forward = f.apply(forward),
-            f_backward = f.apply(backward)
+        return x.map(function (val, i) {
+            var forward = x.slice(0),
+                backward = x.slice(0)
+            forward[i] = val + h[i] / 2.0
+            backward[i] = val - h[i] / 2.0
 
-        return (f_forward - f_backward) / h
+            return (f.apply(null, forward) - f.apply(null, backward)) / h[i]
+        })
     }
 
     function sse(model, rate, time)
@@ -71,12 +79,9 @@
     function sse_interval(model, vol, begin_time)
     {
         var timestep = begin_time[1] - begin_time[0]
-        console.log('timestep = ' + timestep)
         var forecast_cum = begin_time.map(model.cumulative, model)
-        console.log('forecast_cum = ' + forecast_cum)
         forecast_cum.push(
             model.cumulative(begin_time[begin_time.length - 1.0] + timestep))
-        console.log('forecast_cum = ' + forecast_cum)
         return vol.reduce(function (sum, act, i) {
             return sum +
                 Math.pow(act - (forecast_cum[i + 1] - forecast_cum[i]), 2.0)
