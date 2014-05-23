a18b32c30d4a9c78197782ed61a24421b8574803
Derrick W. Turk <dwt@terminusdatascience.com>
2014-05-23 11:50:46 -0500

    First crack at best fitting.

--- bestfit.js
+++ bestfit.js
@@ -1,6 +1,65 @@
 ;(function (ns, undefined) {
     'use strict';
 
+    ns.bestHyperbolicFromRate = function (rate, time)
+    {
+        var best_params = gradient_descent(function (qi, Di, b) {
+            return sse(new ns.Hyperbolic(qi, Di, b), rate, time)
+        }, [rate[0], (rate[1] - rate[0]) / (time[1] - time[0]), 1.5],
+        [0.0, 0.0, 0.0], [1e99, 20.0, 5.0], 0.01, 1e-3, 0.01
+        )
+
+        return new ns.Hyperbolic(
+            best_params[0], best_params[1], best_params[2])
+    }
+
+    ns.bestHyperbolicFromIntervalVolumes = function (vol, time)
+    {
+        var best_params = gradient_descent(function (qi, Di, b) {
+            return sse_interval(new ns.Hyperbolic(qi, Di, b), vol, time)
+        }, [vol[0], (vol[1] - vol[0]) / (time[1] - time[0]), 1.5],
+        [0.0, 0.0, 0.0], [1e99, 20.0, 5.0], 0.01, 1e-3, 0.01
+        )
+
+        return new ns.Hyperbolic(
+            best_params[0], best_params[1], best_params[2])
+    }
+
+    function gradient_descent(f, guess, lower, upper, step, eps, grad_step)
+    {
+        var diff = Infinity,
+            last = f.apply(guess)
+
+        while (diff > eps) {
+            var grad = gradient(f, guess, grad_step)
+            guess = guess.map(function (val, i) {
+                return val - step * grad[i]
+            })
+            guess = guess.map(function (val, i) {
+                if (val < lower[i])
+                    return lower[i]
+                if (val > upper[i])
+                    return upper[i]
+                return val
+            })
+            var next = f.apply(guess)
+            diff = Math.abs(next - last)
+            last = next
+        }
+
+        return guess
+    }
+
+    function gradient(f, x, h)
+    {
+        var forward = x.map(function (i) { return i + h / 2.0 }),
+            backward = x.map(function (i) { return i - h / 2.0 })
+            f_forward = f.apply(forward),
+            f_backward = f.apply(backward)
+
+        return (f_forward - f_backward) / h
+    }
+
     function sse(model, rate, time)
     {
         var forecast = time.map(model.rate, model)
